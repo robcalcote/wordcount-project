31e443b92be165242d993899b44f13b2e90b4faf
Rob <robcalcote@gmail.com>
2018-09-16 23:27:30 -0600

    completed the results page and programmed the site to count the words from an input, and display in highest to lowest order

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
     return render(request, 'home.html')
@@ -8,4 +9,25 @@ def eggs(request):
     return HttpResponse('<h1>YO WHATSUP</h1>')
 
 def count(request):
-    return render(request, 'count.html')
+    #variable that receives the text from fulltext form
+    fulltext = request.GET['fulltext']
+
+    #variable WORDLIST splits the text from fulltext
+    wordlist = fulltext.split()
+
+    worddict = {}
+
+    #This loop will increment words that are added to the library if they have previously appeared
+    for word in wordlist:
+        if word in worddict:
+            #Increase
+            worddict[word] += 1
+        else:
+            #add to the dictionary and don't increment
+            worddict[word] = 1
+
+    sortedwords = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+
+    #the redirect to the results after you're clicked "count the words!"
+    #also creates a new dictionary containing "fulltext", and "count" - gives the amount of words
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist), 'sortedwords':sortedwords})
